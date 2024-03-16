6b3e8013d622330696fefe371b8c8f4bd9ace0df
Tgithinji <t.muraguri420@gmail.com>
2024-03-16 03:18:40 +0300

    Function that returns a string of all elements that start with a given word

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,22 @@
+export default function cleanSet(set, startString) {
+  let text = '';
+
+  // return an empty string if startstring is an empty string
+  if (startString === '') return '';
+
+  // loop through each value in the set to check if it
+  // starts with the given word
+  for (const word of set.values()) {
+    if (word.startsWith(startString)) {
+      // extract the  word after the startsWith
+      const extractedString = word.slice(startString.length);
+      // append the extracted word to a string
+      text += `${extractedString}-`;
+    }
+  }
+
+  // remove trailing '-'
+  text = text.slice(0, -1);
+
+  return text;
+}
