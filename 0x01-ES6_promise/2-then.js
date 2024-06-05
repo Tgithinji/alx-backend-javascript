bbae150a463318d33f3bfadaa445999f44a0e1c1
Tgithinji <t.muraguri420@gmail.com>
2024-06-05 17:45:35 +0300

    Fix typo in resolved body

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => {
     return {
       status: 200,
-      body: 'Success',
+      body: 'success',
     };
   })
   .catch(() => {
@@ -10,5 +10,5 @@ export default function handleResponseFromAPI(promise) {
   })
   .finally(() => {
     console.log('Got a response from the API');
-  });
+  })
 }
