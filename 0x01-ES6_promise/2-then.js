f792f1ee1eefb596d142054e848152b88f7120dd
Tgithinji <t.muraguri420@gmail.com>
2024-06-05 14:37:52 +0300

    Fix resolve handler

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    console.log('Got a response from the API');
     return {
       status: 200,
       body: 'Success',
     };
   })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    }
-    )
+  .catch(() => {
+    return new Error();
+  })
+  .finally(() => {
+    console.log('Got a response from the API');
+  });
 }
