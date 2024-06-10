96b164cc92da4c4542fb36d79edaeb81b8ca454c
Tgithinji <t.muraguri420@gmail.com>
2024-06-10 15:03:57 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,14 @@
 export default function cleanSet(set, startString) {
   let text = '';
 
-  // return an empty string if startstring is an empty string
-  if (startString === '') return '';
+  if (!startString || typeof (startString) !== 'string') {
+    return '';
+  }
 
   // loop through each value in the set to check if it
   // starts with the given word
   for (const word of set.values()) {
-    if (word.startsWith(startString)) {
+    if (word && word.startsWith(startString)) {
       // extract the  word after the startsWith
       const extractedString = word.slice(startString.length);
       // append the extracted word to a string
