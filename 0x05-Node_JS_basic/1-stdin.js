13d500962be392b84a4aacc780e83d7a1e07e690
Tgithinji <t.muraguri420@gmail.com>
2024-10-01 14:02:46 -0400

    Correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 // Display welcome message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
 
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
   process.exit();
 });
