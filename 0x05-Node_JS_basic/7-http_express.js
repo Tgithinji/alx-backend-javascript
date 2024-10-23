4cf75c1a43569e792722f0ca1b11739b1c30d939
Tgithinji <t.muraguri420@gmail.com>
2024-10-23 05:20:23 -0400

    Fix http server

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,75 +1,58 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const app = express();
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      if (lines.length <= 1) {
-        resolve('Number of students: 0');
-        return;
-      }
-
-      const headers = lines[0].split(',');
-      const studentData = lines.slice(1);
-
-      const studentsByField = {};
-
-      studentData.forEach((line) => {
-        const studentInfo = line.split(',');
-        if (studentInfo.length === headers.length) {
-          const field = studentInfo[3];
-          const firstName = studentInfo[0];
-
-          if (!studentsByField[field]) {
-            studentsByField[field] = [];
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const hashtable = {};
+        let students = -1;
+        let result = '';
+        for (const line of lines) {
+          if (line.trim() !== '') {
+            const columns = line.split(',');
+            const field = columns[3];
+            const firstname = columns[0];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
+              }
+              hashtable[field] = [...hashtable[field], firstname];
+            }
+            students += 1;
           }
-          studentsByField[field].push(firstName);
         }
+        result += `Number of students: ${students}\n`;
+        for (const key in hashtable) {
+          if (Object.hasOwnProperty.call(hashtable, key)) {
+            result += `Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}\n`;
+          }
+        }
+        resolve(result);
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
       });
-
-      let output = `Number of students: ${
-        Object.values(studentsByField).reduce((sum, students) => sum + students.length, 0)
-      }\n`;
-
-      for (const [field, students] of Object.entries(studentsByField)) {
-        output += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
-      }
-
-      resolve(output.trim());
-    });
   });
 }
 
-// Express app
-const app = express();
-
-// Root route
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Students route
 app.get('/students', (req, res) => {
-  const databasePath = process.argv[2];
-
-  countStudents(databasePath)
-    .then((message) => {
-      res.setHeader('Content-Type', 'text/plain');
-      res.send(`This is the list of our students\n${message}`);
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
     })
     .catch((error) => {
-      res.status(500).send(error.message);
+      res.status(500).send(`This is the list of our students\n${error.message}`);
     });
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
